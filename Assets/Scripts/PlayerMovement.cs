6e82d00d5fee8a7d1ae4a4090c10a932f7b1d020
Chase <chaseewhitney@gmail.com>
2018-01-19 12:28:57 -0600

    created camera script to follow player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public Rigidbody rb;
+	public float forwardForce = 2000f;
+	public float sidewaysForce = 500f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +15,16 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		rb.AddForce (0,0,2000 * Time.deltaTime);
+		
+		//add a forward force
+		rb.AddForce (0,0,forwardForce * Time.deltaTime);
+
+		if(Input.GetKey("d")){
+			rb.AddForce (sidewaysForce * Time.deltaTime,0,0);
+
+		}
+		if(Input.GetKey("a")){
+			rb.AddForce (-sidewaysForce * Time.deltaTime,0,0);	
+		}
 	}
 }
