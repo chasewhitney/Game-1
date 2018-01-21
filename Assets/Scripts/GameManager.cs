d63c86cfc6e9212e2a7da84842f194b1fc744e79
Chase <chaseewhitney@gmail.com>
2018-01-20 19:34:03 -0600

    almost done with credits

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+	public float restartDelay = 1f;
+	public GameObject levelCompleteUI;
+
+	public void CompleteLevel(){
+		Debug.Log ("Level Won!");
+		levelCompleteUI.SetActive(true);
+	}
+
+	public void EndGame (){
+
+		if (gameHasEnded == false) {
+			Debug.Log ("Game Over");
+			gameHasEnded = true;
+
+			Invoke("Restart", restartDelay);
+		}
+
+	}
+
+	void Restart(){
+
+		SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+
+	}
+
+}
+
+
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public PlayerMovement movement;
+
+	void OnCollisionEnter(Collision collisionInfo) {
+		if (collisionInfo.collider.tag == "Obstacle") {
+		
+			Debug.Log ("You hit an obstacle!");
+
+			movement.enabled = false;
+			//GetComponent<PlayerMovement> ().enabled = false; // same functionality as movement.enabled = false
+
+			FindObjectOfType<GameManager> ().EndGame();
+		}
+
+	}
+
+}
